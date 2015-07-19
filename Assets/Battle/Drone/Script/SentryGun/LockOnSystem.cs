cf157e1edb799bcd5d6b3876397a972d1f1e2e21
sgmt <doku0911@hotmail.co.jp>
2015-07-20 03:04:27 +0900

    垂直方向のロックオンの実装、ならびに制限設定

--- Assets/Battle/Drone/Script/SentryGun/LockOnSystem.cs
+++ Assets/Battle/Drone/Script/SentryGun/LockOnSystem.cs
@@ -10,16 +10,22 @@ public class LockOnSystem : MonoBehaviour {
     [SerializeField, Tooltip("１フレームあたりに回転する最大角度")]
     float rotateEulerMaxY = 20.0f;
 
+    /// <summary>
+    /// 調節用の変数
+    /// 銃が正面を向いていないので90度傾けて考えます。
+    /// </summary>
     float addEuler = 90;
-
-    float eulerAngleY = 0.0f;
 
 	// Update is called once per frame
 	void Update () 
     {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            SetTargetWithTag("Player");
+        }
+
         if (target == null) return;
 
-        eulerAngleY = transform.eulerAngles.y;
 
         Vartical();
 
@@ -34,21 +40,41 @@ public class LockOnSystem : MonoBehaviour {
     float upperEulerMax = 9.0f;
 
 
-
+    /// <summary>
+    /// 垂直方向のエイム
+    /// </summary>
     void Vartical()
     {
         /// 自分より下にいる敵は狙わない
         if (target.position.y < transform.position.y) return;
 
+        var horizontalDistance = Mathf.Sqrt((target.position.x - transform.position.x) * (target.position.x - transform.position.x) + (target.position.z - transform.position.z) * (target.position.z - transform.position.z));
+
+        var addEulerAngleZ = Mathf.Atan2(horizontalDistance, target.position.y - transform.position.y) * 180 / Mathf.PI;
+
+        /// addEulerAngleZをマイナスにし、さらに90度傾ける
+        /// ↓の短縮
+        /// addEulerAngleZ *= -1;
+        /// addEulerAngleZ += addEuler;
+        addEulerAngleZ = addEuler - addEulerAngleZ;
+        
+        if (addEulerAngleZ > upperEulerMax)
+        {
+            addEulerAngleZ = upperEulerMax;
+        }
+
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, addEulerAngleZ);
 
     }
 
+    /// <summary>
+    /// 水平方向のエイム
+    /// </summary>
     void Horizontal()
     {
         var addEulerAngle = Mathf.Atan2(target.position.x - transform.position.x, target.position.z - transform.position.z) * 180 / Mathf.PI;
 
-        var deltaAngle = Mathf.DeltaAngle(eulerAngleY, addEulerAngle - addEuler);
-
+        var deltaAngle = Mathf.DeltaAngle(transform.eulerAngles.y, addEulerAngle - addEuler);
 
         if (Mathf.Abs(deltaAngle) > 3.0f)
         {
@@ -61,8 +87,26 @@ public class LockOnSystem : MonoBehaviour {
                 transform.Rotate(new Vector3(0f, -rotateEulerMaxY * Mathf.PI / 180, 0f));
             }
         }
-
-	
-
     }
+
+    /// <summary>
+    /// ターゲットを設定する。
+    /// </summary>
+    /// <param name="_target">ターゲットのTransform</param>
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+    
+    /// <summary>
+    /// ターゲットを設定する。
+    /// </summary>
+    /// <param name="_tag">ターゲットのタグ</param>
+    public void SetTargetWithTag(string _tag)
+    {
+        target = GameObject.FindGameObjectWithTag(_tag).GetComponent<Transform>();
+    }
+
+
+
 }
